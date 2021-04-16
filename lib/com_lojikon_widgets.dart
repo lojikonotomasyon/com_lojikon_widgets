d840fa5ad9b14bb4a04a179fd3e94d275cd9bf48
Yusuf Demiröz <yusuf@lojikon.com>
2021-04-17 00:57:47 +0300

    Text widget example

--- lib/com_lojikon_widgets.dart
+++ lib/com_lojikon_widgets.dart
@@ -1,7 +1,3 @@
 library com_lojikon_widgets;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:com_lojikon_widgets/text.dart';
---
+++ lib/text.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+export 'package:com_lojikon_widgets/text.dart';
+
+class LText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Sample LText",
+      ),
+    );
+  }
+}
